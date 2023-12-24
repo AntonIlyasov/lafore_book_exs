79ecb9de7aa120e45455d8e188fa3e95712a2eda
AntonIlyasov <ilyasov.toni.97@mail.ru>
2023-12-24 20:49:06 +0300

    first commit

---
+++ build/CMakeFiles/laf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laf.dir/main.cpp.o"
+  "laf"
+  "laf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

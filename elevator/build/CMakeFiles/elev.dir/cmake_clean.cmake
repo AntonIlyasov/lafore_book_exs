cca1cbb26a26b06b4a2ab45eeab393920a033b0e
Anton Ilyasov <ilyasov.toni.97@mail.ru>
2024-08-02 12:24:05 +0300

    stable

---
+++ elevator/build/CMakeFiles/elev.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elev.dir/elev.cpp.o"
+  "CMakeFiles/elev.dir/elev_app.cpp.o"
+  "CMakeFiles/elev.dir/msoftcon.cpp.o"
+  "elev"
+  "elev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/elev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
